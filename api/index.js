8fdc88a215e2f483b7625683ea56c9a1fa9a8107
ARUNTANGUDU <134259335+ARUNTANGUDU@users.noreply.github.com>
2024-07-04 12:15:15 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';//for database usage
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,3 +15,15 @@ const app =express();
 app.listen(3000,()=>{
     console.log('server is running in port 3000');
 });
+
+app.use('/api/user',userRouter)
+
+//route will be presented in route folder and then the (req,res) will be in controller folder
+
+
+// app.get('/test',(req,res)=>{
+//     // res.send('hello world');
+//     res.json({
+//         'message':"hello worls"
+//     })
+// });
